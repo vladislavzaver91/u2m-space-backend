2ea07358229b3414284472c3e60cc875a852f9d0
vladislavzaver91 <vladislavzaver91@gmail.com>
2025-04-30 16:17:01 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(
 	cors({
-		origin: [process.env.FRONTEND_URL, 'http://localhost:3001'],
+		origin: process.env.FRONTEND_URL || 'http://localhost:3001',
 		credentials: true,
 		methods: ['GET', 'POST', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
